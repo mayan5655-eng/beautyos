ba913e48b96704b4c6458cca051fbc1d400217dd
Maayan Babai <Mayan5655@gmail.com>
2026-05-11 00:57:18 +0300

    Fix __dirname error with standalone output

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'standalone',
+  serverExternalPackages: ['@supabase/ssr', '@supabase/supabase-js'],
 };
 
 export default nextConfig;
